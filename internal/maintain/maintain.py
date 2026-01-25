f203e72caf9256bcb1444d29c6b72cdc7d822488
DiamondOsas <diamondosazuwa@gmail.com>
2026-01-25 11:25:28 +0100

    mka the friciton smaller

--- internal/maintain/maintain.py
+++ internal/maintain/maintain.py
@@ -16,25 +16,25 @@ def _get_documents_folder() -> Path:
     return Path(os.environ['USERPROFILE']) / "Documents"
 
 
-def _validate_csv_header(file_path: Path) -> bool:
-    """
-    Validates if the first row (header) of a given CSV file matches the EXPECTED_HEADER.
+# def _validate_csv_header(file_path: Path) -> bool:
+#     """
+#     Validates if the first row (header) of a given CSV file matches the EXPECTED_HEADER.
 
-    Args:
-        file_path (Path): The path to the CSV file to validate.
+#     Args:
+#         file_path (Path): The path to the CSV file to validate.
 
-    Returns:
-        bool: True if the header matches, False otherwise or if the file cannot be read.
-    """
-    try:
-        with open(file_path, mode="r", newline="", encoding="utf-8") as f:
-            reader = csv.reader(f)
-            header = next(reader, None)
-            if header == EXPECTED_HEADER:
-                return True
-    except (Exception, IOError, StopIteration, csv.Error) as e:
-        print(f"Error reading header File: {e}")
-        return False
+#     Returns:
+#         bool: True if the header matches, False otherwise or if the file cannot be read.
+#     """
+#     try:
+#         with open(file_path, mode="r", newline="", encoding="utf-8") as f:
+#             reader = csv.reader(f)
+#             header = next(reader, None)
+#             if header == EXPECTED_HEADER:
+#                 return True
+#     except (Exception, IOError, StopIteration, csv.Error) as e:
+#         print(f"Error reading header File: {e}")
+#         return False
 
 # ==============================================================
 def perform_daily_backup():
@@ -46,29 +46,32 @@ try:
     #GEt app data location on windows
     app_data = os.getenv('APPDATA')
     
-    backup_root = app_data / "MTU_BACKUP"
+    app_data_path = Path(app_data)
+    backup_root = app_data_path / "MTU_BACKUP"
 
     #Ensuring that the flder exists
 
     backup_root.mkdir(parents=True, exist_ok= True)
 
     today_str = datetime.now().strftime("%d-%m-%Y")
+
+    
     today_backup_path  =  backup_root / today_str
+    
     #If the today_backup folder does not exist 
     if not today_backup_path.exists():
         print(f"Creatng backup today at {today_backup_path}")
-        today_backup_path.mkdir()
+        
 
         source = Path("db")
         #Create it 
         if source.exists():
-            shutil.copy2(source ,today_backup_path)
+            shutil.copytree(source ,today_backup_path)
             print("Daily backup successful")
         else:
             print(f"Error copying file to  {today_backup_path}")
     else:
-        print(f"Error creating backup")
-
+        print(f"Backup skipped: Folder for today already exists.")
 except Exception as e:
     print(f"Error performing daily backup: {e}")
 
@@ -105,7 +108,7 @@ def maintain_student_data_files():
         shutil.copy2(internal_file_path, external_data_dir)
     
 
-    print("At least done just check ")
+    
 
 
 if __name__ == "__main__":
